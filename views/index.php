8312b14cc2e3fde55d1d66a896c4de02c2e4b574
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-05-13 01:11:47 +0700

    refactor section works layout

--- views/index.php
+++ views/index.php
@@ -145,28 +145,30 @@ $project = $projectRead->getAll();
 					</div>
 					<!-- project list -->
 					<div id="project-list">
-						<div class="col-12 col-md-6 col-xl-4 mt-4">
+						<div class="row">
 							<?php $i = 1; ?>
 							<?php foreach ($project as $projectRow) { ?>
-								<div class="card">
-									<img src="../assets/img/<?= htmlspecialchars($projectRow['project_photo']) ?>" class="card-img-top" alt="Project-<?= $i; ?>" />
-									<div class="card-body">
-										<h5 class="card-title"><?= htmlspecialchars($projectRow['project_category']) ?></h5>
-										<p class="card-text"><?= htmlspecialchars($projectRow['project_details']) ?></p>
-									</div>
-									<div class="dropdown d-flex justify-content-end pb-2 pe-2">
-										<button
-											id="dropdown-menu-button"
-											class="btn btn-circular-secondary btn-round dropdown-toggle"
-											type="button"
-											data-bs-toggle="dropdown"
-											aria-expanded="false">
-											<i class="bi bi-list"></i>
-										</button>
-										<ul class="dropdown-menu" aria-labelledby="dropdown-menu-button">
-											<li><a class="dropdown-item" href="update.html">Edit</a></li>
-											<li><a class="dropdown-item" href="">Delete</a></li>
-										</ul>
+								<div class="col-12 col-md-6 col-xl-4 mt-4">
+									<div class="card">
+										<img src="../assets/img/<?= htmlspecialchars($projectRow['project_photo']) ?>" class="card-img-top" alt="Project-<?= $i; ?>" />
+										<div class="card-body">
+											<h5 class="card-title"><?= htmlspecialchars($projectRow['project_category']) ?></h5>
+											<p class="card-text"><?= htmlspecialchars($projectRow['project_details']) ?></p>
+										</div>
+										<div class="dropdown d-flex justify-content-end pb-2 pe-2">
+											<button
+												id="dropdown-menu-button"
+												class="btn btn-circular-secondary btn-round dropdown-toggle"
+												type="button"
+												data-bs-toggle="dropdown"
+												aria-expanded="false">
+												<i class="bi bi-list"></i>
+											</button>
+											<ul class="dropdown-menu" aria-labelledby="dropdown-menu-button">
+												<li><a class="dropdown-item" href="update.html">Edit</a></li>
+												<li><a class="dropdown-item" href="">Delete</a></li>
+											</ul>
+										</div>
 									</div>
 								</div>
 								<?php $i++; ?>
